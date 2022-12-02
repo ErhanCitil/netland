bf99d373326fcb4e4a6950ec03f0968655d3782c
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-02 16:39:09 +0100

    Added static settings in settings.py

--- netland/settings.py
+++ netland/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',  
             'NAME': 'netland',  
             'USER': 'root',  
-            'PASSWORD': 'bit_academy',  
+            'PASSWORD': '',  
             'HOST': '127.0.0.1',  
             'PORT': '3306',  
             'OPTIONS': {  
@@ -123,7 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
