efcc63fce6f46c1e870827178d4d63380fe35818
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-05 16:06:35 +0100

    Cleaned the serie application

--- serie/urls.py
+++ serie/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from .models import *
 from .views import *
-from . import views
 
 urlpatterns = [
         path('', Index.as_view(), name='index'),
-        path('serie/<int:id>', DetailSeries.as_view(), name='detailserie'),
-        path('updateserie/<int:id>', UpdateSerie.as_view(), name='updateserie'),
-        path('updateserie/updateseriedetails/<int:id>', views.updateseriedetails, name='updateseriedetails'),
+        path('serie/<int:pk>', DetailSerie.as_view(), name='detailserie'),
+        path('updateserie/<int:pk>', UpdateSerie.as_view(), name='updateserie'),
         path('deleteserie/<int:id>', DeleteSerie.as_view(), name='deleteserie'),
 ]
--- serie/views.py
+++ serie/views.py
@@ -1,15 +1,11 @@
-from django.shortcuts import get_object_or_404
 from .models import *
-from django.http import HttpResponseRedirect
-
-from django.views.generic.list import ListView
-
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views import generic
 from django.urls import reverse_lazy
+from movie.models import *
+from .forms import *
 
-from movie.models import Movies
 # Create your views here.
-class Index(ListView):
+class Index(generic.ListView):
     model = Series
     template_name = 'index.html'
     context_object_name = 'series'
@@ -19,36 +15,17 @@ class Index(ListView):
         context['movies'] = Movies.objects.all()
         return context
 
-class DetailSeries(ListView):
+class DetailSerie(generic.DetailView):
     model = Series
     template_name = 'detailserie.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['object'] = Series.objects.get(id=self.kwargs['id'])
-        return context
-
-class UpdateSerie(ListView):
+class UpdateSerie(generic.UpdateView):
     model = Series
     template_name = 'updateserie.html'
+    form_class = SerieForm
+    success_url = reverse_lazy('index')
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['object'] = Series.objects.get(id=self.kwargs['id'])
-        return context
-
-def updateseriedetails(request, id):
-    obj = get_object_or_404(Series, pk=id)
-    obj.title = request.POST['title']
-    obj.rating = request.POST['rating']
-    obj.summary = request.POST['summary']
-    obj.has_won_awards = request.POST['has_won_awards']
-    obj.seasons = request.POST['seasons']
-    obj.country = request.POST['country']
-    obj.save()
-    return HttpResponseRedirect("/")
-
-class DeleteSerie(DeleteView):
+class DeleteSerie(generic.DeleteView):
     model = Series
     template_name = 'deleteserie.html'
     success_url = reverse_lazy('index')
