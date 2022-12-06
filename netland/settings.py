d6edbff9e252180ae21daf1c7cb4da2bff88dfde
¨Erhan¨ <erhan.citil@maykin.nl>
2022-12-06 15:58:13 +0100

    Buttons for add serie and add movie

--- netland/settings.py
+++ netland/settings.py
@@ -79,15 +79,8 @@ WSGI_APPLICATION = 'netland.wsgi.application'
 DATABASES = {  
         'default': {  
             'TEST': {
-            'ENGINE': 'django.db.backends.mysql',  
-            'NAME': 'test_netland',  
-            'USER': 'root',  
-            'PASSWORD': '',  
-            'HOST': '127.0.0.1',  
-            'PORT': '3306',  
-            'OPTIONS': {  
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-            }  
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
             },
             'ENGINE': 'django.db.backends.mysql',  
             'NAME': 'netland',  
