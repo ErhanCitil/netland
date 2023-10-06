41feb8de22c2e7b92d166e663eb8c6af5893d1d2
Erhan <erhan.citil@maykinmedia.nl>
2023-10-06 16:40:03 +0200

    🗃️  Added postgresql instead of mysql

--- netland/settings.py
+++ netland/settings.py
@@ -79,15 +79,12 @@ WSGI_APPLICATION = 'netland.wsgi.application'
 
 DATABASES = {  
         'default': {  
-            'ENGINE': 'django.db.backends.mysql',  
+            'ENGINE': 'django.db.backends.postgresql',  
             'NAME': 'netland',  
-            'USER': 'root',  
-            'PASSWORD': '',  
+            'USER': 'erhan',  
+            'PASSWORD': 'test',  
             'HOST': '127.0.0.1',  
-            'PORT': '3306',  
-            'OPTIONS': {  
-                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-            }  
+            'PORT': '5432',  
         }  
     }  
 
